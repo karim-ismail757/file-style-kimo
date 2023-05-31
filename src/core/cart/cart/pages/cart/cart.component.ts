7891a9eca4440d4c383f05e8cbc263c62530afe0
IbrahimNour <ebrahimnour186@gmail.com>
2023-05-31 12:19:44 +0300

    test

--- src/core/cart/cart/pages/cart/cart.component.ts
+++ src/core/cart/cart/pages/cart/cart.component.ts
@@ -5,34 +5,23 @@ import { ProviderServicesService } from 'src/provider-services.service';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss']
+  styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent implements OnInit {
-  
-  totalPrice!:number;
-  totalTime!:any;
+  totalPrice!: number;
+  totalTime!: any;
   ngOnInit(): void {
     this.getCartData();
-
-
   }
-  cart:any=[];
-  constructor(private provider:ProviderServicesService){
+  cart: any = [];
+  constructor(private provider: ProviderServicesService) {}
 
-
-  }
-
-  getCartData(){
-    this.provider.getServicesToCart().subscribe((response)=>{
-      this.cart=response.Result;
+  getCartData() {
+    this.provider.getServicesToCart().subscribe((response) => {
+      this.cart = response.Result;
       debugger;
-      this.totalPrice=response?.Result?.TotalPrice;
-      this.totalTime=response?.Result?.Booking?.Time;
-
-    })
+      this.totalPrice = response?.Result?.TotalPrice;
+      this.totalTime = response?.Result?.Booking?.Time;
+    });
   }
-
-
-
-
 }
